85edc18e07cef606b28d5c4a1f4ce664c0c7f9cc
hyunjin <leehj0110@kakao.com>
2021-05-25 16:28:23 +0900

    리액트 공부

---
+++ 리덕스 라이브러리 이해하기/vanilla-redux/index.js
@@ -0,0 +1,97 @@
+// console.log("hello parcel")
+
+import {createStore} from 'redux';
+import { sortAndDeduplicateDiagnostics } from 'typescript';
+
+
+
+const store = createStore(reducer);
+
+const render = ()=> {
+    const state = store.getState();
+    if (state.toggle) {
+        divToggle.classList.add('active');
+
+    }else {
+        divToggle.classList.remove('active');
+
+    }
+
+    counter.innerText = state.counter;
+
+}
+
+
+render();
+store.subscribe(render)
+
+const divToggle = document.querySelector('.toggle')
+
+
+const counter = document.querySelector('h1');
+
+
+
+
+const btnIncrease = document.querySelector('#increase')
+
+
+const btnDecrease = document.querySelector('#decrease')
+
+
+
+
+
+
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+const toggleSwitch = () => ({type: TOGGLE_SWITCH});
+const increase = difference => ({type: INCREASE, difference});
+const decrease = () => ({ type: DECREASE})
+
+
+const initailState = {
+    toggle: false,
+    counter: 0
+}
+
+function reducer(state=initailState, action) {
+    switch (action.type) {
+        case TOGGLE_SWITCH:
+            return {
+                ...state,
+                toggle: !state.toggle
+            }
+
+        case INCREASE:
+            return {
+                ...state,
+                counter: state.counter + action.difference
+            }
+        case DECREASE:
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+
+        default:
+            return state;
+    }
+}
+
+
+
+divToggle.onClick = () => {
+    store.dispatch(toggleSwitch());
+}
+
+btnIncrease.onClick = () => {
+    store.dispatch(increase(1));
+
+}
+
+btnDecrease.onClick = () => {
+    store.dispatch(decrease())
+}
